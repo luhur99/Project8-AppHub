2a09b41ba8530236da122238bb0c02262805873c
[elmony] <luhur@budikaryateknologi.com>
2026-01-17 23:40:13 +0700

    [elmony] Temporarily removed GalaxyBackground from Index.tsx to isolate 'dyad' error. - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import React from 'react';
-import GalaxyBackground from "@/components/GalaxyBackground";
 import ServiceCard from "@/components/ServiceCard";
 import DailyVerse from "@/components/DailyVerse";
-import { MadeWithDyad } from "@/components/made-with-elmony";
+import { MadeWithDyad } = from "@/components/made-with-elmony";
 import { Rocket, ShieldCheck, Settings, Lightbulb } from 'lucide-react';
 
 const Index = () => {
@@ -41,7 +40,7 @@ const Index = () => {
 
   return (
     <div className="relative min-h-screen flex flex-col items-center justify-center p-4 overflow-hidden">
-      <GalaxyBackground />
+      {/* <GalaxyBackground /> */} {/* Temporarily removed */}
 
       <div className="relative z-10 text-center mb-12 mt-16">
         <h1 className="text-5xl md:text-6xl font-extrabold text-white drop-shadow-lg mb-4">
