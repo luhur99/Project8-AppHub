149d3ee485939804af1981c0d3e74e9d9c1f1447
luhur99 <luhur@budikaryateknologi.com>
2026-03-01 18:09:20 +0700

    Update favicon and made-with-elmony component

--- src/components/made-with-elmony.tsx
+++ src/components/made-with-elmony.tsx
@@ -2,12 +2,12 @@ export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center">
       <a
-        href="https://www.elmony.com/"
+        href="https://www.budikaryateknologi.com/"
         target="_blank"
         rel="noopener noreferrer"
         className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
       >
-        Made with Elmony
+        Made with Bismillah
       </a>
     </div>
   );
